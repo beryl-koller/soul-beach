7ed2ea34bcd9d0ee6dd15801f995c805f7105aac
Beryl Armand <beryl.armand@gmail.com>
2021-06-18 16:30:20 +0200

    Site updated to 959232e

--- javascripts/app.js
+++ javascripts/app.js
@@ -12706,3 +12706,31 @@ return jQuery;
 
 
 
+
+let circle = document.getElementById("circle");
+
+const onMouseMove = (e) => {
+  circle.style.left = (e.pageX) + "px";
+  circle.style.top = (e.pageY) + "px";
+};
+document.addEventListener("mousemove", onMouseMove);
+
+
+
+const constellations = document.querySelectorAll(".constellation")
+
+const dancingCursor = () => {
+    document.getElementById("dancing").style.display = "block";
+    document.getElementById("still").style.display = "none";
+};
+
+const stillCursor = () => {
+  document.getElementById("dancing").style.display = "none";
+  document.getElementById("still").style.display = "block";
+};
+
+constellations.forEach((constellation) => {
+    constellation.addEventListener("mouseenter", dancingCursor)
+    constellation.addEventListener("mouseleave", stillCursor)
+})
+;
